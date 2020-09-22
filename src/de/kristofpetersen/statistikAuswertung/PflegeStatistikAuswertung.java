8fb770e5e2509a1cb628c3c5a96d3299253cbbd6
KristofP3t <71642970+KristofP3t@users.noreply.github.com>
2020-09-22 22:11:50 +0200

    Updates

--- src/de/kristofpetersen/statistikAuswertung/PflegeStatistikAuswertung.java
+++ src/de/kristofpetersen/statistikAuswertung/PflegeStatistikAuswertung.java
@@ -1,14 +1,22 @@
 package de.kristofpetersen.statistikAuswertung;
 
 import java.awt.BorderLayout;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.border.TitledBorder;
 
 public class PflegeStatistikAuswertung extends JFrame{
 	/**
@@ -17,6 +25,9 @@ public class PflegeStatistikAuswertung extends JFrame{
 	private static final long serialVersionUID = 2144419201198548508L;
 	private JComboBox<String> statistikAuswahl;
 	private String[] statistik = {"Gutachtenverarbeitung", "Antragsinterview"};
+	private JLabel einstufungsGutachten, aussortiert, vollUndTeilautomatisiert, vollautomatisiert, teilautomatisiert, unplausiblePunktwerte,
+		erledigungsart, keineZugelasseneKombi, befristet, unterbrechungVorhanden, eilantragVorhanden, vorkasseVorhanden, keinGueltigesVvh,
+		sonstige, gesamtquote, quoteVollautomatisiert, quoteTeilautomatisiert;
 	
 	public PflegeStatistikAuswertung(String titel) {
 		super(titel);
@@ -25,7 +36,8 @@ public class PflegeStatistikAuswertung extends JFrame{
 		statistikAuswahl = new JComboBox<String>(statistik);
 		setSize(600, 600);
 		setLocationRelativeTo(null);
-		add(statistikAuswahl, BorderLayout.CENTER);
+		add(statistikAuswahl, BorderLayout.NORTH);
+		add(statistikDatstellungErstellen(), BorderLayout.CENTER);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setVisible(true);
 	}
@@ -35,8 +47,11 @@ public class PflegeStatistikAuswertung extends JFrame{
 		public void actionPerformed(ActionEvent e) {
 			if(e.getActionCommand().equals("beenden"))
 				System.exit(0);
-			if(e.getActionCommand().equals("suchen"))
-				dateiLaden();
+			if(e.getActionCommand().equals("suchen")) {
+				if (JOptionPane.showConfirmDialog(null, "Soll eine Statistik für \"" + statistikAuswahl.getSelectedItem().toString() + "\" erstellt werden?", 
+						statistikAuswahl.getSelectedItem().toString(), JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE) == JOptionPane.YES_OPTION)
+					erstelleStatistik();
+			}
 		}
 		
 	}
@@ -57,8 +72,125 @@ public class PflegeStatistikAuswertung extends JFrame{
 		this.setJMenuBar(menue);
 	}
 	
-	private void dateiLaden() {
+	private JPanel statistikDatstellungErstellen() {
+		JPanel tempPanel = new JPanel();
+		tempPanel.setBorder(new TitledBorder("Statistik"));
+		tempPanel.setLayout(new GridLayout(0,1));
+		einstufungsGutachten = new JLabel("Einstufungsgutachten: ");
+		aussortiert = new JLabel("Nicht maschinell Verarbeitbar: ");
+		vollUndTeilautomatisiert = new JLabel("Voll- und Teilautomatisiert: ");
+		vollautomatisiert = new JLabel("Vollautomatisiert: ");
+		teilautomatisiert = new JLabel("Teilautomatisiert: ");
+		unplausiblePunktwerte = new JLabel("Unplausible Punktwerte: ");
+		erledigungsart = new JLabel("Erledigungsart: ");
+		keineZugelasseneKombi = new JLabel("Keine zugelassene Kombi: ");
+		befristet = new JLabel("Befristung: ");
+		unterbrechungVorhanden = new JLabel("Unterbrechung vorhanden: ");
+		eilantragVorhanden = new JLabel("Eilantrag vorhanden: ");
+		vorkasseVorhanden = new JLabel("Vorkasse vorhanden: ");
+		keinGueltigesVvh = new JLabel("Kein gültiges Versicherungsverhältnis: ");
+		sonstige = new JLabel("Sonstige: ");
+		gesamtquote = new JLabel("Verarbeitungsquote gesamt: ");
+		quoteVollautomatisiert = new JLabel("Verarbeitungsquote Vollautomatisiert: ");
+		quoteTeilautomatisiert = new JLabel("Verarbeitungsquote Teilautomatisiert: ");
 		
+		tempPanel.add(einstufungsGutachten);
+		tempPanel.add(aussortiert);
+		tempPanel.add(vollUndTeilautomatisiert);
+		tempPanel.add(vollautomatisiert);
+		tempPanel.add(teilautomatisiert);
+		tempPanel.add(unplausiblePunktwerte);
+		tempPanel.add(erledigungsart);
+		tempPanel.add(keineZugelasseneKombi);
+		tempPanel.add(befristet);
+		tempPanel.add(unterbrechungVorhanden);
+		tempPanel.add(eilantragVorhanden);
+		tempPanel.add(vorkasseVorhanden);
+		tempPanel.add(keinGueltigesVvh);
+		tempPanel.add(sonstige);
+		tempPanel.add(gesamtquote);
+		tempPanel.add(quoteVollautomatisiert);
+		tempPanel.add(quoteTeilautomatisiert);
+		
+		return tempPanel;
+	}
+	
+	private void erstelleStatistik() {
+		StatistikDialoge dialoge = new StatistikDialoge();
+		File datei = dialoge.oeffnenDialogZeigen();
+		String headLine = "";
+		String lineString = "";
+		int gesamtGutachten = 0;
+		int anzahlAussortiert = 0;
+		int anzahlVollUndTeilAutomatisiert = 0;
+		int anzahlVollautomatisiert = 0;
+		int anzahlTeilautomatisiert = 0;
+		int anzahlUnplausiblePunktwerte = 0;
+		int anzahlErledigungsart = 0;
+		int anzahlKeineZugelasseneKombi = 0;
+		int anzahlBefristet = 0;
+		int anzahlUnterbrechungVorhanden = 0;
+		int anzahlEilantragVorhanden = 0;
+		int anzahlVorkasseVorhanden = 0;
+		int anzahlKeinGueltigesVvh = 0;
+		int anzahlSonstige = 0;
+		double anzahlGesamtquote = 0;
+		double anzahlVollquote = 0;
+		double anzahlTeilquote = 0;
+		
+		try {
+			BufferedReader reader = new BufferedReader(new FileReader(datei));
+			headLine = reader.readLine();
+			lineString = reader.readLine();
+			while (lineString != null) {
+				gesamtGutachten++;
+				String[] aktuelleLine = lineString.split(";");
+				if(!aktuelleLine[8].isEmpty()) {
+					if (aktuelleLine[8].equals("UNPLAUSIBLE_PUNKTWERTE")) {
+						anzahlUnplausiblePunktwerte++;
+					}
+					if (aktuelleLine[8].equals("ERLEDIGUNGSART")) {
+						anzahlErledigungsart++;
+					}
+					if (aktuelleLine[8].equals("KEINE_ZUGELASSENEN_KOMBI_GUTACHTENART_PFLEGEEINSTUFUNG_PFLEGEGRAD")) {
+						anzahlKeineZugelasseneKombi++;
+					}
+					if (aktuelleLine[8].equals("BEFRISTET")) {
+						anzahlBefristet++;
+					}
+					if (aktuelleLine[8].equals("UNTERBRECHUNG_VORHANDEN")) {
+						anzahlUnterbrechungVorhanden++;
+					}
+					if (aktuelleLine[8].equals("EILANTRAG_VORHANDEN")) {
+						anzahlEilantragVorhanden++;
+					}
+					if (aktuelleLine[8].equals("VORKASSE_VORHANDEN")) {
+						anzahlVorkasseVorhanden++;
+					}
+					if (aktuelleLine[8].equals("")) {
+						anzahlKeinGueltigesVvh++;
+					}
+				}
+					
+				
+				lineString = reader.readLine();
+			} 
+		}
+		catch (IOException e) {
+			e.printStackTrace();
+			}
+		einstufungsGutachten.setText(einstufungsGutachten.getText() + gesamtGutachten);
+		aussortiert.setText(aussortiert.getText() + anzahlAussortiert);
+		unplausiblePunktwerte.setText(unplausiblePunktwerte.getText() + anzahlUnplausiblePunktwerte);
+		erledigungsart.setText(erledigungsart.getText() + anzahlErledigungsart);
+		keineZugelasseneKombi.setText(keineZugelasseneKombi.getText() + anzahlKeineZugelasseneKombi);
+		befristet.setText(befristet.getText() + anzahlBefristet);
+		unterbrechungVorhanden.setText(unterbrechungVorhanden.getText() + anzahlUnterbrechungVorhanden);
+		eilantragVorhanden.setText(eilantragVorhanden.getText() + anzahlEilantragVorhanden);
+		vorkasseVorhanden.setText(vorkasseVorhanden.getText() + anzahlVorkasseVorhanden);
+		keinGueltigesVvh.setText(keinGueltigesVvh.getText() + anzahlKeinGueltigesVvh);
+		//sonstige
+		}
+
 	}
 
-}
--- src/de/kristofpetersen/statistikAuswertung/StatistikDialoge.java
+++ src/de/kristofpetersen/statistikAuswertung/StatistikDialoge.java
@@ -7,7 +7,7 @@ import javax.swing.JFileChooser;
 import javax.swing.filechooser.FileFilter;
 
 public class StatistikDialoge {
-	//eine Innere Klasse für den Filter
+	
 	class MeinFilter extends FileFilter{
 
 		@Override
@@ -23,32 +23,26 @@ public class StatistikDialoge {
 
 		@Override
 		public String getDescription() {
-			return "Textdateien";
+			return "CSV-Dateien";
 		}
 	}
 	
-	//die Methode zeigt den Öffnendialog für eine Datei
 	public File oeffnenDialogZeigen() {
-		//einen neuen Dialog erzeugen
 		JFileChooser oeffnenDialog= new JFileChooser();
 		oeffnenDialog.setFileFilter(new MeinFilter());
 		oeffnenDialog.setAcceptAllFileFilterUsed(false);
-		//den DIalog anzeugen und den Status holen
 		int status = oeffnenDialog.showOpenDialog(null);
-		//wurde auf Öffnen geklickt, dann die ausgewählte Datei als Zeichenkette ausgeben
 		if(status == JFileChooser.APPROVE_OPTION)
 			return(oeffnenDialog.getSelectedFile());
 		else
 			return null;
 	}
+	
 	public File speichernDialogZeigen() {
 		JFileChooser speichernDialog = new JFileChooser();
 		speichernDialog.setFileFilter(new MeinFilter());
 		speichernDialog.setAcceptAllFileFilterUsed(false);
-		//den Dialog anzeigen und den Status holen
-		//hier wird die Methode showSaveDialog() aufgerufen
 		int status= speichernDialog.showSaveDialog(null);
-		//wurde auf Save geklickt, dann die ausgewählte Datei als Typ File zurückliefern
 		if(status== JFileChooser.APPROVE_OPTION)
 			return (speichernDialog.getSelectedFile());
 		else 
